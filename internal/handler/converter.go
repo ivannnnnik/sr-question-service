0411f0984a6ce260726136a3f6621da3c075b098
Nikolaev Ivan <akovalev.ren.088@gmail.com>
2026-03-11 22:16:20 +0300

    feature: add converter for  List method

--- internal/handler/converter.go
+++ internal/handler/converter.go
@@ -16,3 +16,20 @@ func QuestionToProto(question *model.Question) *questionv1.Question{
 		CreatedAt: question.CreatedAt.Format(time.RFC3339),
 	}
 }
+
+
+func QuestionsToProto(questions []model.Question) []*questionv1.Question{
+	result := []*questionv1.Question{}
+	
+	for _, question := range questions{
+		result = append(result,
+			&questionv1.Question{
+				Id: question.ID,
+				Title: question.Title,
+				Category: question.Category,
+				Difficulty: question.Difficulty,
+				CreatedAt: question.CreatedAt.Format(time.RFC3339),
+		})
+	}
+	return result
+}
